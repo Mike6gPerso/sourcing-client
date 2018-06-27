4e200b970795373c5996e2a746d8d99f2e355372
Mike6gPerso <XNDN0238@orange.com>
2018-06-27 17:23:06 +0200

    Routing

---
+++ src/app/app-routing/app-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes }  from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+import { SearchComponent } from '../search/search.component';
+import { PageNotFoundComponent } from '../page-not-found/page-not-found.component';
+import { DataVizComponent } from '../data-viz/data-viz.component';
+
+const appRoutes: Routes = [
+  { path: 'search', component: SearchComponent },
+  { path: 'dataviz',        component: DataVizComponent },
+  { path: '',   redirectTo: '/search', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent },
+  //{ path: '**', component: PageNotFoundComponent }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
+  ],
+    exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,28 +3,36 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 import { LayoutModule } from '@angular/cdk/layout';
+import { CustomMaterialModule } from './custom-material/custom-material.module';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing/app-routing.module';
+
 import { NavComponent } from './nav/nav.component';
 import { SearchComponent } from './search/search.component';
-import { CustomMaterialModule } from './custom-material/custom-material.module';
 
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { DataVizComponent } from './data-viz/data-viz.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    SearchComponent
+    SearchComponent,
+    PageNotFoundComponent,
+    DataVizComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     LayoutModule,
-    CustomMaterialModule,
     FormsModule,
-    ReactiveFormsModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    CustomMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
