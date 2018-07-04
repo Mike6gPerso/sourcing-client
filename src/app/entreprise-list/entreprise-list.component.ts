7a15b7383368139908f92fb0438e2a49fd49444c
Mike6gPerso <XNDN0238@orange.com>
2018-07-04 16:31:55 +0200

    Using Mat Table - still need to use the stream properly

--- src/app/entreprise-list/entreprise-list.component.ts
+++ src/app/entreprise-list/entreprise-list.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild, AfterViewInit } from '@angular/core';
 import { map } from 'rxjs/operators';
 import { Subscription } from 'rxjs';
-import { MatPaginator, MatTableDataSource } from '@angular/material';
+import { MatSort, MatPaginator, MatTableDataSource } from '@angular/material';
 
 import { EntrepriseService } from '../entreprise.service';
 import { Entreprise } from '../entreprise';
@@ -11,33 +11,48 @@ import { Entreprise } from '../entreprise';
 	templateUrl: './entreprise-list.component.html',
 	styleUrls: ['./entreprise-list.component.css']
 })
-export class EntrepriseListComponent implements OnInit {
+export class EntrepriseListComponent implements OnInit, AfterViewInit {
 
 	entreprises: any;
 	entrepriseSubscription: Subscription;
-	displayedColumns: string[] = ['entreprise', 'context'];
+	displayedColumns: string[] = ['entreprise', 'context', 'descriptif'];
 	dataSource : MatTableDataSource<Entreprise>; //= new MatTableDataSource<>();
 
 	@ViewChild(MatPaginator) paginator: MatPaginator;
+	@ViewChild(MatSort) sort: MatSort;
 
 	constructor(private entrepriseService: EntrepriseService) { }
 
 	ngOnInit() {
 		this.getEntreprisesList();
-		this.dataSource.paginator = this.paginator;
+	}
+
+	ngAfterViewInit() {
+		// If the user changes the sort order, reset back to the first page.
+    	this.sort.sortChange.subscribe(() => this.paginator.pageIndex = 0);
 	}
 
 	getEntreprisesList() {
   	 // Use snapshotChanges().map() to store the key
-  	 this.entrepriseSubscription = this.entrepriseService.getPaginatedEntreprises(0,10).snapshotChanges().pipe(
+  	 this.entrepriseSubscription = this.entrepriseService
+  	 	.getEntreprisesList()
+  	 	//.getPaginatedEntreprises(0,30)
+  	 	.snapshotChanges().pipe(
   	 	map(changes => 
   	 		changes.map(c => ({ key: c.payload.key, ...c.payload.val() }))
   	 		)
   	 	).subscribe(entreprises => {
   	 		this.entreprises = entreprises;
+  	 		//console.log("entreprises: " + JSON.stringify(entreprises));
   	 		this.dataSource = new MatTableDataSource<Entreprise>(entreprises);
+  	 		this.dataSource.paginator = this.paginator;
+  	 		this.dataSource.sort = this.sort;
   	 	});
   	//subscription.unsubscribe();
+  }
+
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
   ngOnDestroy() {
